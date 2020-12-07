a7618b58ae67fc635e3fbcc8fc0ab6c0dd4b2d38
Daniel Monzer <daniel.monzer23@ycdsbk12.ca>
2020-12-07 18:33:02 +0000

    Livehack#1 problems 1 and 2 completed

--- problem1.py
+++ problem1.py
@@ -1,1 +1,19 @@
+'''
+-------------------
+Name: problem1.py
 
+Purpose: Caculates degrees in fahrenheit from celsius
+
+Author: Daniel Monzer
+
+Created: 07/12/20
+-------------------
+'''
+#Display what this program is used for
+print("***Celsius to fahrenheit Converter***")
+#Create the input for the user to type their answer
+celsius = float(input("Type the temperature in celsius: "))
+#Calculate the degrees in fahrenheit
+fahrenheit = (9/5 * celsius) + 32
+#Print the degrees in fahrenheit
+print("The temperature is",fahrenheit,"degrees fahrenheit")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,19 @@
+'''
+-------------------
+Name: problem2.py
 
+Purpose: Calculates number of chickens split between the students and Mr.Fabroa
+
+Author: Daniel Monzer
+
+Created: 07/12/20
+-------------------
+'''
+#Create an input for the user to type the amount of students
+students = int(input("How many students are in the class?: "))
+#Create an input for the user to type the amount of chicken
+chicken = int(input("How many pieces of chicken are there?: "))
+#Print how much chicken each student will get
+print("Each student will get",chicken // students,"pieces of chicken")
+#Print how much chicken will Mr.Fabroa get
+print("Mr. Fabroa will get",chicken % students,"pieces of chicken")
